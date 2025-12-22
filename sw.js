c6ba639fdc1e45ff9ce6f6ab828d0d8066a4ff94
dredog <aaasbury@gmail.com>
2025-12-21 23:39:26 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,89 @@
-const CACHE_NAME = 'minemuncher-v3';
+const CACHE_NAME = 'minemuncher-v4';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://fonts.gstatic.com/s/pressstart2p/v15/e3t4euO8T-267oIAQAu6jDQyK3nVivM.woff2'
+  './icon-512.png'
 ];
 
+// External resources to cache
+const externalResources = [
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'
+];
+
+// Install event - cache all resources
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        // Cache local files first
+        return cache.addAll(urlsToCache)
+          .then(() => {
+            // Try to cache external resources
+            return Promise.allSettled(
+              externalResources.map(url => 
+                fetch(url).then(response => {
+                  if (response.ok) {
+                    return cache.put(url, response);
+                  }
+                }).catch(() => {})
+              )
+            );
+          });
+      })
       .then(() => self.skipWaiting())
   );
 });
 
+// Activate event - clean up old caches
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(names => 
-      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames
+            .filter(name => name !== CACHE_NAME)
+            .map(name => caches.delete(name))
+        );
+      })
+      .then(() => self.clients.claim())
   );
 });
 
+// Fetch event - serve from cache, fallback to network
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request)
-        .then(res => {
-          if (res.status === 200) {
-            const clone = res.clone();
-            caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-          }
-          return res;
-        })
-      ).catch(() => caches.match('./index.html'))
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        
+        return fetch(event.request)
+          .then(response => {
+            // Don't cache non-successful responses
+            if (!response || response.status !== 200) {
+              return response;
+            }
+            
+            // Clone the response
+            const responseToCache = response.clone();
+            
+            // Cache the new resource
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          })
+          .catch(() => {
+            // If both cache and network fail, return the cached index.html
+            return caches.match('./index.html');
+          });
+      })
   );
 });
